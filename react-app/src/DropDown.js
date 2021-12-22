c0faaf421691525fd6ea0ca874550d1c78491d10
Peter Bagnall <peter.bagnall@ul.com>
2021-12-22 11:59:13 +0000

    Got changed events working in React. Wrapped the webcomponent in a React component and manually forwarded events.

---
+++ react-app/src/DropDown.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default class DropDown extends React.Component {
+    constructor(props) {
+        super(props);
+        this.ref = React.createRef();
+    }
+
+    componentDidMount() {
+        if (this.props.onChange) {
+            this.ref.current.addEventListener('change', e => this.props.onChange(e));
+        }
+    }
+
+    render() {
+        return (
+            <ul-dropdown ref={this.ref}>{this.props.children}</ul-dropdown>
+        );
+    }
+}
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,27 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import DropDown from "./DropDown.js";
 
-class MultipleDropDowns extends React.Component {
-    render() {
-        return (
-            <div className='toolbar'>
-                <ul-dropdown>
-                    <option slot='option' value='1'>One</option>
-                    <option slot='option' value='2'>Two</option>
-                    <option slot='option' value='3'>Three</option>
-                </ul-dropdown>
-                <ul-dropdown>
-                    <option slot='option' value='4'>Four</option>
-                    <option slot='option' value='5'>Five</option>
-                    <option slot='option' value='6'>Six</option>
-                </ul-dropdown>
-            </div>
-        );
-    }
+const App = () => {
+    const [first, setFirst] = useState("no selection");
+    const [second, setSecond] = useState("no selection");
+
+    return (
+        <>
+        <h1>React App using a WebComponent</h1>
+        <div className='layout'>
+            <div id='firstOutput'>{first}</div>
+            <div id='secondOutput'>{second}</div>
+            <DropDown onChange={e => { setFirst(e.detail) } }>
+                <option slot='option' value='1'>One</option>
+                <option slot='option' value='2'>Two</option>
+                <option slot='option' value='3'>Three</option>
+            </DropDown>
+            <DropDown onChange={e => { setSecond(e.detail) } }>
+                <option slot='option' value='4'>Four</option>
+                <option slot='option' value='5'>Five</option>
+                <option slot='option' value='6'>Six</option>
+            </DropDown>
+        </div>
+        </>
+    );
 }
 
 ReactDOM.render(
-    <MultipleDropDowns />,
+    <App />,
     document.getElementById('root')
   );
