03f636f1ba0b99f5a3d904bec6662af3047d629e
Peter Bagnall <peter.bagnall@ul.com>
2021-12-22 17:35:57 +0000

    Added angular-app, which shows the web component but does not yet listen to events.

---
+++ angular-app/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import './DropDown.js';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'angular-app';
+}
+
