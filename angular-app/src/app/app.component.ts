7a4275c1cfc0cb95a6b2aa9d606e7b97d1b35ce0
Peter Bagnall <peter.bagnall@ul.com>
2021-12-22 17:59:27 +0000

    Angular app now responding to events from the web component.

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -7,6 +7,20 @@ import './DropDown.js';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  @ViewChild("dropdown1") dropdown1!: ElementRef;
+  @ViewChild("dropdown2") dropdown2!: ElementRef;
+  @ViewChild("output1") output1!: ElementRef; 
+  @ViewChild("output2") output2!: ElementRef;
   title = 'angular-app';
+
+  updateOutput(e: CustomEvent, target: ElementRef): boolean {
+    target.nativeElement.innerHTML = e.detail;
+    return false;
+  }
+
+  ngAfterViewInit() {
+    this.dropdown1.nativeElement.addEventListener("change", (e: any) => this.updateOutput(e, this.output1));
+    this.dropdown2.nativeElement.addEventListener("change", (e: any) => this.updateOutput(e, this.output2));
+  }
 }
 
